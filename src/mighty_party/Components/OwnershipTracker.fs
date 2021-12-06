d73d420c97d7fa68cbcfcb0f9be70f538220b4ec
bdimpe@gmail <imaginarydevelopment@gmail.com>
2021-12-06 18:19:45 -0500

    ownership tracking is doing much better

--- src/mighty_party/Components/OwnershipTracker.fs
+++ src/mighty_party/Components/OwnershipTracker.fs
@@ -1,5 +1,7 @@
 namespace App.MightyParty.Components.OwnershipTracker
 
+open Fable.Import
+
 open App.MightyParty.Schema
 
 type OwnershipDatum = TrackedHero
@@ -9,17 +11,40 @@ type Props = {
     SetOwned: App.MightyParty.Schema.TrackedHeroCollection option -> unit
 }
 
-type State ={
+type State = {
     HeroesOwned: App.MightyParty.Schema.TrackedHeroCollection
 }
 
 type Msg =
     | OwnedClicked of int
+    | BindLevelChange of int * int
 
 module OwnTracking =
     open Elmish
     open Feliz
     let heroes = App.MightyParty.Hero.heroes
+    type BindInfo = {   TrackedHero:TrackedHero
+                        Soulbind:(Soulbind*Map<string,TrackedHero option> ) option
+                    }
+
+    let getHeroBindInfo (hero:Hero) (owned:TrackedHeroCollection) =
+        match owned |> Map.tryFind hero.ID with
+        | Some th ->
+            match hero.Soulbinds |> List.tryItem th.BindLevel with
+            | Some sbs ->
+                let bo =
+                    sbs.Requirements
+                    |> Seq.map(fun x ->
+                        x, owned |> Map.tryPick(fun _ v -> if  v.Name = x && v.Owned then Some v else None)
+                    )
+                    |> Map.ofSeq
+
+                Some {
+                    TrackedHero = th
+                    Soulbind = Some (sbs,bo)
+                }
+            | None -> Some { TrackedHero = th; Soulbind = None }
+        | None -> None
 
     let changeOwned (owned:TrackedHeroCollection,i) =
         match owned |> Map.tryFind i, heroes |> Seq.tryFind(fun h -> h.ID = i) with
@@ -28,6 +53,14 @@ module OwnTracking =
         | None, None ->
             eprintfn "Failed to find hero ID %i to changeOwned" i
             owned
+
+    let updateHero (owned:TrackedHeroCollection) i f =
+        match owned |> Map.tryFind i, heroes |> Seq.tryFind(fun h -> h.ID = i) with
+        | Some o, Some h ->
+            let updated = f(o,h)
+            owned
+            |> Map.add i updated
+        | _ -> owned
 
     let init(serializer) =
         let getOwned,setOwned = App.MightyParty.Hero.makeOwnedProp serializer
@@ -47,32 +80,100 @@ module OwnTracking =
             let nextOwned = changeOwned (state.HeroesOwned,x)
             props.SetOwned (Some nextOwned)
             { state with HeroesOwned = nextOwned }, Cmd.none
+        | BindLevelChange(id,v) ->
+            let nextOwned =
+                updateHero state.HeroesOwned id (fun (o,_h) ->
+                    {o with BindLevel = v}
+                )
+            { state with HeroesOwned = nextOwned}, Cmd.none
 
-
-    let view (_,state) dispatch =
-        let findOwnStatus id =
-                let result = state.HeroesOwned |> Map.tryFind id
-                result
-        heroes
-        |> Array.map(fun hero ->
-            let ht = state.HeroesOwned |> Map.tryFind hero.ID
-            let isOwned = ht |> Option.map(fun ht -> ht.Owned) |> Option.defaultValue false
-            Html.li[
-                prop.onClick (fun _ -> dispatch (OwnedClicked hero.ID))
-                prop.custom("data-owned", string isOwned)
-                prop.children [
-                    // not sure if this a referrer or http/https or some other issue
-                    // Html.img [
-                    //     let img = sprintf "http%s" hero.Image.[5..]
-                    //     prop.src img
-                    // ]
-                    Html.input[
-                        prop.type' "checkbox"
-                        prop.isChecked isOwned
+    let renderSoulbinds sb binds =
+        let sbs =
+            binds
+            |> Map.toSeq
+            |> Seq.map(fun (name, btho) ->
+                match btho with
+                | Some bth ->
+                    let text =
+                            if sb.ReqLvl <= bth.Level then
+                                name
+                            else sprintf "%s %i" name (bth.Level - sb.ReqLvl)
+                    Html.li [
+                        prop.style [ style.display.inlineElement]
+                        prop.text text
                     ]
-                    Html.span[
-                        prop.text hero.Name
+                | None -> Html.li [
+                    prop.style [
+                        style.display.inlineElement
+                        style.listStyleType.circle
+                        style.listStylePosition.inside
+                    ]
+                    prop.text (sprintf "%s" name)]
+            )
+        sbs
+    let renderHeroView dispatch heroesOwned hero =
+        let hbi = getHeroBindInfo hero heroesOwned
+        let isOwned = hbi |> Option.map(fun x -> x.TrackedHero.Owned) |> Option.defaultValue false
+
+        Html.li[
+            prop.custom("data-owned", string isOwned)
+            prop.children [
+                // not sure if this a referrer or http/https or some other issue
+                // Html.img [
+                //     let img = sprintf "http%s" hero.Image.[5..]
+                //     prop.src img
+                // ]
+                Html.span [
+                    prop.onClick (fun _ -> dispatch (OwnedClicked hero.ID))
+                    prop.children[
+                        Html.input[
+                            prop.type' "checkbox"
+                            prop.isChecked isOwned
+                        ]
+                        Html.span[
+                            prop.classes [
+                                hero.Rarity
+                            ]
+                            prop.text hero.Name
+                        ]
                     ]
                 ]
+                match hbi with
+                | Some hbi when hbi.TrackedHero.Owned ->
+                    let sbs =
+                        match hbi.Soulbind with
+                        | Some (sb,binds) ->
+                            renderSoulbinds sb binds
+                        | None -> Seq.empty
+
+                    Html.input [
+                        prop.title "BindLevel"
+                        prop.defaultValue hbi.TrackedHero.BindLevel
+                        prop.onChange (fun (x:string) -> dispatch (BindLevelChange(hbi.TrackedHero.ID, int x)))
+                        prop.type' "number"
+                        prop.custom("max","4")
+                        prop.custom("min","0")
+                        prop.ariaValueMax 4
+                        prop.ariaValueMin 0
+                    ]
+                    Html.span [
+                        prop.text "For Next"
+                        prop.children [
+                            Html.ul [
+                                prop.style [ style.display.inlineElement]
+                                prop.children[
+                                    yield! sbs
+                                ]
+                            ]
+                        ]
+                    ]
+                | _ -> ()
             ]
+        ]
+    let view (_,state:State) dispatch =
+        heroes
+        |> Seq.sortBy(fun x -> x.Rarity <> "Legendary", x.Name)
+        |> Seq.map(fun hero ->
+            renderHeroView dispatch state.HeroesOwned hero
         )
+        |> Array.ofSeq
--- src/mighty_party/Components/OwnershipTracker.fs.js
+++ src/mighty_party/Components/OwnershipTracker.fs.js
@@ -1,15 +1,17 @@
 import { toString, Union, Record } from "../../fable_modules/fable-library.3.6.3/Types.js";
-import { union_type, record_type, option_type, lambda_type, class_type, int32_type, unit_type } from "../../fable_modules/fable-library.3.6.3/Reflection.js";
-import { TrackedHero, TrackedHero$reflection } from "../Schema.fs.js";
+import { tuple_type, string_type, union_type, record_type, option_type, lambda_type, class_type, int32_type, unit_type } from "../../fable_modules/fable-library.3.6.3/Reflection.js";
+import { TrackedHero, Soulbind$reflection, TrackedHero$reflection } from "../Schema.fs.js";
 import { makeOwnedProp, heroes } from "../Hero.fs.js";
-import { FSharpMap__get_Item, add, tryFind } from "../../fable_modules/fable-library.3.6.3/Map.js";
-import { tryFind as tryFind_1 } from "../../fable_modules/fable-library.3.6.3/Seq.js";
-import { printf, toConsoleError } from "../../fable_modules/fable-library.3.6.3/String.js";
+import { toSeq, FSharpMap__get_Item, add, tryPick, ofSeq, tryFind } from "../../fable_modules/fable-library.3.6.3/Map.js";
+import { tryItem } from "../../fable_modules/fable-library.3.6.3/List.js";
+import { sortBy, empty, singleton, append, delay, toList, tryFind as tryFind_1, map } from "../../fable_modules/fable-library.3.6.3/Seq.js";
+import { join, toText, printf, toConsoleError } from "../../fable_modules/fable-library.3.6.3/String.js";
 import { Cmd_none } from "../../fable_modules/Fable.Elmish.3.1.0/cmd.fs.js";
-import { map } from "../../fable_modules/fable-library.3.6.3/Array.js";
-import { map as map_1, defaultArg } from "../../fable_modules/fable-library.3.6.3/Option.js";
 import { createElement } from "react";
+import { map as map_1, defaultArg } from "../../fable_modules/fable-library.3.6.3/Option.js";
 import { Interop_reactApi } from "../../fable_modules/Feliz.1.53.0/Interop.fs.js";
+import { parse } from "../../fable_modules/fable-library.3.6.3/Int32.js";
+import { compareArrays } from "../../fable_modules/fable-library.3.6.3/Util.js";
 
 export class Props extends Record {
     constructor(GetOwned, SetOwned) {
@@ -41,15 +43,53 @@ export class Msg extends Union {
         this.fields = fields;
     }
     cases() {
-        return ["OwnedClicked"];
+        return ["OwnedClicked", "BindLevelChange"];
     }
 }
 
 export function Msg$reflection() {
-    return union_type("App.MightyParty.Components.OwnershipTracker.Msg", [], Msg, () => [[["Item", int32_type]]]);
+    return union_type("App.MightyParty.Components.OwnershipTracker.Msg", [], Msg, () => [[["Item", int32_type]], [["Item1", int32_type], ["Item2", int32_type]]]);
 }
 
 export const OwnTracking_heroes = heroes;
+
+export class OwnTracking_BindInfo extends Record {
+    constructor(TrackedHero, Soulbind) {
+        super();
+        this.TrackedHero = TrackedHero;
+        this.Soulbind = Soulbind;
+    }
+}
+
+export function OwnTracking_BindInfo$reflection() {
+    return record_type("App.MightyParty.Components.OwnershipTracker.OwnTracking.BindInfo", [], OwnTracking_BindInfo, () => [["TrackedHero", TrackedHero$reflection()], ["Soulbind", option_type(tuple_type(Soulbind$reflection(), class_type("Microsoft.FSharp.Collections.FSharpMap`2", [string_type, option_type(TrackedHero$reflection())])))]]);
+}
+
+export function OwnTracking_getHeroBindInfo(hero, owned) {
+    const matchValue = tryFind(hero.ID, owned);
+    if (matchValue == null) {
+        return void 0;
+    }
+    else {
+        const th = matchValue;
+        const matchValue_1 = tryItem(th.BindLevel, hero.Soulbinds);
+        if (matchValue_1 == null) {
+            return new OwnTracking_BindInfo(th, void 0);
+        }
+        else {
+            const sbs = matchValue_1;
+            const bo = ofSeq(map((x) => [x, tryPick((_arg1, v) => {
+                if ((v.Name === x) && v.Owned) {
+                    return v;
+                }
+                else {
+                    return void 0;
+                }
+            }, owned)], sbs.Requirements));
+            return new OwnTracking_BindInfo(th, [sbs, bo]);
+        }
+    }
+}
 
 export function OwnTracking_changeOwned(owned, i) {
     const matchValue = [tryFind(i, owned), tryFind_1((h) => (h.ID === i), OwnTracking_heroes)];
@@ -69,6 +109,33 @@ export function OwnTracking_changeOwned(owned, i) {
     }
 }
 
+export function OwnTracking_updateHero(owned, i, f) {
+    const matchValue = [tryFind(i, owned), tryFind_1((h) => (h.ID === i), OwnTracking_heroes)];
+    let pattern_matching_result, h_1, o;
+    if (matchValue[0] != null) {
+        if (matchValue[1] != null) {
+            pattern_matching_result = 0;
+            h_1 = matchValue[1];
+            o = matchValue[0];
+        }
+        else {
+            pattern_matching_result = 1;
+        }
+    }
+    else {
+        pattern_matching_result = 1;
+    }
+    switch (pattern_matching_result) {
+        case 0: {
+            const updated = f([o, h_1]);
+            return add(i, updated, owned);
+        }
+        case 1: {
+            return owned;
+        }
+    }
+}
+
 export function OwnTracking_init(serializer) {
     const patternInput = makeOwnedProp(serializer);
     const setOwned = patternInput[1];
@@ -80,32 +147,134 @@ export function OwnTracking_init(serializer) {
 }
 
 export function OwnTracking_update(props, state, msg) {
-    const x = msg.fields[0] | 0;
-    const nextOwned = OwnTracking_changeOwned(state.HeroesOwned, x);
-    props.SetOwned(nextOwned);
-    return [new State(nextOwned), Cmd_none()];
+    if (msg.tag === 1) {
+        const v = msg.fields[1] | 0;
+        const id = msg.fields[0] | 0;
+        const nextOwned_1 = OwnTracking_updateHero(state.HeroesOwned, id, (tupledArg) => {
+            const o = tupledArg[0];
+            const _h = tupledArg[1];
+            return new TrackedHero(o.ID, o.Owned, o.Level, v, o.Name);
+        });
+        return [new State(nextOwned_1), Cmd_none()];
+    }
+    else {
+        const x = msg.fields[0] | 0;
+        const nextOwned = OwnTracking_changeOwned(state.HeroesOwned, x);
+        props.SetOwned(nextOwned);
+        return [new State(nextOwned), Cmd_none()];
+    }
 }
 
-export function OwnTracking_view(_arg1, state, dispatch) {
-    const findOwnStatus = (id) => {
-        const result = tryFind(id, state.HeroesOwned);
-        return result;
-    };
-    return map((hero) => {
-        const ht = tryFind(hero.ID, state.HeroesOwned);
-        const isOwned = defaultArg(map_1((ht_1) => ht_1.Owned, ht), false);
-        return createElement("li", {
-            onClick: (_arg1_1) => {
+export function OwnTracking_renderSoulbinds(sb, binds) {
+    const sbs = map((tupledArg) => {
+        const name = tupledArg[0];
+        const btho = tupledArg[1];
+        if (btho == null) {
+            return createElement("li", {
+                style: {
+                    display: "inline",
+                    listStyleType: "circle",
+                    listStylePosition: "inside",
+                },
+                children: toText(printf("%s"))(name),
+            });
+        }
+        else {
+            const bth = btho;
+            let text;
+            if (sb.ReqLvl <= bth.Level) {
+                text = name;
+            }
+            else {
+                const arg20 = (bth.Level - sb.ReqLvl) | 0;
+                text = toText(printf("%s %i"))(name)(arg20);
+            }
+            return createElement("li", {
+                style: {
+                    display: "inline",
+                },
+                children: text,
+            });
+        }
+    }, toSeq(binds));
+    return sbs;
+}
+
+export function OwnTracking_renderHeroView(dispatch, heroesOwned, hero) {
+    const hbi = OwnTracking_getHeroBindInfo(hero, heroesOwned);
+    const isOwned = defaultArg(map_1((x) => x.TrackedHero.Owned, hbi), false);
+    return createElement("li", {
+        ["data-owned"]: toString(isOwned),
+        children: Interop_reactApi.Children.toArray(Array.from(toList(delay(() => append(singleton(createElement("span", {
+            onClick: (_arg1) => {
                 dispatch(new Msg(0, hero.ID));
             },
-            ["data-owned"]: toString(isOwned),
             children: Interop_reactApi.Children.toArray([createElement("input", {
                 type: "checkbox",
                 checked: isOwned,
             }), createElement("span", {
+                className: join(" ", [hero.Rarity]),
                 children: hero.Name,
             })]),
-        });
-    }, OwnTracking_heroes);
+        })), delay(() => {
+            let hbi_1;
+            let pattern_matching_result, hbi_2;
+            if (hbi != null) {
+                if ((hbi_1 = hbi, hbi_1.TrackedHero.Owned)) {
+                    pattern_matching_result = 0;
+                    hbi_2 = hbi;
+                }
+                else {
+                    pattern_matching_result = 1;
+                }
+            }
+            else {
+                pattern_matching_result = 1;
+            }
+            switch (pattern_matching_result) {
+                case 0: {
+                    let sbs;
+                    const matchValue = hbi_2.Soulbind;
+                    if (matchValue == null) {
+                        sbs = empty();
+                    }
+                    else {
+                        const sb = matchValue[0];
+                        const binds = matchValue[1];
+                        sbs = OwnTracking_renderSoulbinds(sb, binds);
+                    }
+                    return append(singleton(createElement("input", {
+                        title: "BindLevel",
+                        defaultValue: hbi_2.TrackedHero.BindLevel,
+                        onChange: (ev) => {
+                            dispatch(new Msg(1, hbi_2.TrackedHero.ID, parse(ev.target.value, 511, false, 32)));
+                        },
+                        type: "number",
+                        max: "4",
+                        min: "0",
+                        ["aria-valuemax"]: 4,
+                        ["aria-valuemin"]: 0,
+                    })), delay(() => singleton(createElement("span", {
+                        children: "For Next",
+                        children: Interop_reactApi.Children.toArray([createElement("ul", {
+                            style: {
+                                display: "inline",
+                            },
+                            children: Interop_reactApi.Children.toArray(Array.from(toList(delay(() => sbs)))),
+                        })]),
+                    }))));
+                }
+                case 1: {
+                    return empty();
+                }
+            }
+        })))))),
+    });
+}
+
+export function OwnTracking_view(_arg1, state, dispatch) {
+    return Array.from(map((hero) => OwnTracking_renderHeroView(dispatch, state.HeroesOwned, hero), sortBy((x) => [x.Rarity !== "Legendary", x.Name], OwnTracking_heroes, {
+        Compare: (x_1, y) => compareArrays(x_1, y),
+    })));
 }
 
